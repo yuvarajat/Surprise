e2557f5a4c1b69a0b99c633eb1fe15946aaabf30
Yuvaraja T <54367219+yuvarajat@users.noreply.github.com>
2024-01-29 05:17:11 +0530

    Final Commit

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const wishEl = document.getElementById('wish');
 const wishTypeEl = document.getElementById("wish-type");
 const imageEl = document.getElementById("img-container");
 const wishSendBtn = document.getElementById("send-msg-btn");
+const clickMeBtn = document.getElementById("click-me");
+const heartGifEl = document.getElementById("heart-gif");
+const luEl = document.getElementById("lu");
 
 wishTypeEl.focus();
 
@@ -14,7 +17,12 @@ wishSendBtn.addEventListener("click", function() {
     };
     setTimeout(function() {
         imageEl.style.display = "none";
-        
+        setTimeout(function() {
+            clickMeBtn.style.display = "block";
+            heartGifEl.style.display = "block";
+            luEl.style.display = "block";
+        }, 200);
     }, 4000);
+    
 });
 
